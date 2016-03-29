f11a145c934768fec5549049d4027e528d62bc2d
BiXiaoQiang <bqq8540@163.com>
2016-03-29 13:22:39 +0800

    运行时项目创建

---
+++ BXQRuntime/BXQRuntime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BXQRuntime
+//
+//  Created by 英策网络科技 on 16/3/29.
+//  Copyright © 2016年 英策网络科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
